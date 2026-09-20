60d20330a0401bfe05a861730ceaa2dd12662816
derek-martell <7073248@gmail.com>
2026-09-20 02:02:33 -0500

    Actualiza materiales de Macroeconomía III (automático)

--- datos.js
+++ datos.js
@@ -1,6 +1,6 @@
 /* GENERADO por publicar.py. No editar a mano: se sobrescribe. */
 window.MATERIALES = {
- "actualizado": "19/09/2026",
+ "actualizado": "20/09/2026",
  "ciclos": [
   {
    "id": "2026-II",
